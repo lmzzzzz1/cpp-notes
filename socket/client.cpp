c91ed95375bb42eb24f5e868e631ec9ae4640e6d
lmzzzzz1 <lmzzzzz1@163.com>
2021-02-17 16:32:02 +0800

    简单的socket编程，接下来看http_server

---
+++ socket/client.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+int main1(){
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serv_addr.sin_port = htons(1234);
+    connect(sock,(struct sockaddr*) &serv_addr, sizeof(serv_addr));
+
+    char buf[40];
+    read(sock,buf, sizeof(buf)-1);
+
+    printf("Message from server: %s\n", buf);
+    close(sock);
+}
